62903d86c68d616fb98968724a28d762784d2b98
saintsim <simon.scarpa@gmail.com>
2020-12-06 22:00:01 +0900

    Condensed day 6

--- src/day6/part1.py
+++ src/day6/part1.py
@@ -2,22 +2,16 @@
 
 
 def customs_form_sum(lines):
-    total = 0
-    group = set()
+    total, group = 0, set()
     for line in lines:
         if line == "\n":
             total += len(group)
             group = set()
-            continue
-        for token in line:
-            if token == "\n":
-                continue
-            group.add(token)
-    total += len(group)
-    return total
+        else:
+            [group.add(token) for token in line if token != '\n']
+    return total + len(group)
 
 
 if __name__ == '__main__':
     with open('input', 'r') as file:
-        lines = file.readlines()
-        print('Result: ' + str(customs_form_sum(lines)))
+        print('Result: ' + str(customs_form_sum(file.readlines())))
--- src/day6/part2.py
+++ src/day6/part2.py
@@ -2,31 +2,24 @@
 
 
 def size_of_intersection(lists):
-    result = set(lists[0])
-    for a_list in lists[1:]:
-        result.intersection_update(a_list)
-    return len(result)
+    intersection_set = set(lists[0])
+    [intersection_set.intersection_update(this_list) for this_list in lists[1:]]
+    return len(intersection_set)
 
 
 def customs_form_sum(lines):
-    total = 0
-    group, groups = [], []
+    total, groups = 0, []
     for line in lines:
+        group = set()
         if line == "\n":
             total += size_of_intersection(groups)
             groups = []
-            continue
-        for token in line:
-            if token == "\n":
-                continue
-            group.append(token)
-        groups.append(group)
-        group = []
-    total += size_of_intersection(groups)
-    return total
+        else:
+            [group.add(token) for token in line if token != '\n']
+            groups.append(group)
+    return total + size_of_intersection(groups)
 
 
 if __name__ == '__main__':
     with open('input', 'r') as file:
-        lines = file.readlines()
-        print('Result: ' + str(customs_form_sum(lines)))
+        print('Result: ' + str(customs_form_sum(file.readlines())))
