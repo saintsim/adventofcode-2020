5b96fbfc4e8eb9a5fdee99cb37c57ba8dc025092
saintsim <simon.scarpa@gmail.com>
2020-12-10 23:15:25 +0900

    List has native count so use that so we don't need to import Counter

--- src/day10/part1.py
+++ src/day10/part1.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-from collections import Counter
-
-
 def jolt_differences(lines):
     lines.sort()
     # lines[0] to attach to the charging outlet
@@ -10,7 +7,7 @@ def jolt_differences(lines):
     diffs.extend([num - lines[i] for i, num in enumerate(lines[1:])])
     # 3 because your device's built-in adapter is always 3 higher than the highest adapter
     diffs.append(3)
-    return Counter(diffs)[1] * Counter(diffs)[3]
+    return diffs.count(1) * diffs.count(3)
 
 
 if __name__ == '__main__':
